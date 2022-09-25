d90df09d49edcd47caf60e76b354a4e755b7d3ab
kevin-park01 <jkphyke08@gmail.com>
2022-09-25 00:00:12 -0700

    Basic EC2 Implmentation

--- controller.py
+++ controller.py
@@ -1,9 +1,12 @@
 from sqs import SQS
 from s3 import S3
+from ec2 import EC2
 
 
 def main():
-    
+    '''
+    # Examples:
+
     sqs = SQS()
     # Input queue
     sqs.send_request('input test')
@@ -14,6 +17,19 @@ def main():
 
     s3 = S3()
     s3.store_image('./images/test_0.JPEG', 'test_0.JPEG')    
+    
+    ec2 = EC2()
+    # Start web instance
+    ec2.start_web_instance()
+    # Create, start, and terminate an app instance
+    inst = ec2.create_app_instance()
+    ec2.start_instance(inst.id)
+    ec2.terminate_instance(inst)
+    # Stop web instance
+    ec2.stop_web_instance()
+    '''
+
+    
 
 
 if __name__ == '__main__':
---
+++ ec2.py
@@ -0,0 +1,51 @@
+import boto3
+import config
+
+
+# AWS EC2 class
+class EC2:
+
+    # Initialize EC2 client and resource
+    def __init__(self):
+        # EC2 Client
+        self.ec2_client = boto3.client(
+            service_name='ec2', 
+            region_name=config.REGION, 
+            aws_access_key_id=config.AWS_ACCESS_KEY_ID, 
+            aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+        )
+        # EC2 Resource
+        self.ec2_resource = boto3.resource(
+            service_name='ec2', 
+            region_name=config.REGION, 
+            aws_access_key_id=config.AWS_ACCESS_KEY_ID, 
+            aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+        )
+
+
+    # Create an app instance and return it
+    def create_app_instance(self):
+        instance = self.ec2_resource.create_instances(
+            ImageId=config.MODEL_IMAGE_ID,
+            MinCount=1,
+            MaxCount=1
+        )
+        return instance[0]
+
+
+    # Start an instance
+    def start_instance(self, instance_id):
+        self.ec2_client.start_instances(InstanceIds=[instance_id])
+        print(f'Successfully started instance {instance_id}')
+
+    
+    # Stop an instance
+    def stop_instance(self, instance_id):
+        self.ec2_client.stop_instances(InstanceIds=[instance_id])
+        print(f'Successfully stopped instance {instance_id}')
+
+
+    # Terminate an instance
+    def terminate_instance(self, instance):
+        instance.terminate()
+        print(f'Successfully terminated instance {instance.id}')
